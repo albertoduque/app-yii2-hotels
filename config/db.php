ee093591ede2167df85c0e42b511331a30505bca
Alberto Duque <duque.alberto@gmail.com>
2019-12-16 21:26:59 -0500

    add db config

--- config/db.php
+++ config/db.php
@@ -1,10 +1,16 @@
 <?php
 
+$server = 'ec2-174-129-24-148.compute-1.amazonaws.com';
+$username = 'xusvbyovpywkup';
+$password = '9fdc354e3dabdfdd49b568861125854d8c0e171162dfe8481ef6b15ea91be57f';
+$db = 'dfa4pqm9hvqdnp';
+
+
 return [
   'class' => 'yii\db\Connection',
-  'dsn' => 'pgsql:host=localhost;dbname=hotelesDB',
-  'username' => 'postgres',
-  'password' => 'admin123',
+  'dsn' => 'pgsql:host='.$server.';dbname='.$db,
+  'username' => $username,
+  'password' => $password,
   'charset' => 'utf8',
   'schemaMap' => [
       'pgsql' => [
